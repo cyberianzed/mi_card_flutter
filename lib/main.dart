d8fd7aef9545adf894bbe3e9600f2fb2b800e53d
cyberianzed <josekjames5@gmail.com>
2021-05-25 01:03:21 +0530

    columns and  rows

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,33 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.lightBlue[600],
         body: SafeArea(
-          child: Container(
-            height: 100,
-            width: 100,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(30.0),
-            color: Colors.greenAccent,
-             child: Text('Hello')
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.yellow,
+                  child: Text('Container 1')),
+              SizedBox(
+                width: 50,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.white,
+                child: Text('Container 2'),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.green,
+                child: Text('Container 3'),
+              )
+            ],
           ),
         ),
       ),
