b0a9dce5b2810390d2fb18f003b71406104cbbbf
Jose K James <josekjames5@gmail.com>
2021-08-13 10:45:41 +0530

    casual

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Folder\flutter\mi_card_flutter"
+export "FLUTTER_ROOT=C:\Users\josek\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\Folder\flutter\mi_card_flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
